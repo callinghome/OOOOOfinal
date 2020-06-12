1681fb1c07e36d28b0269e60b512ddba4b691f5f
callinghome <61899408+callinghome@users.noreply.github.com>
2020-06-12 23:25:51 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,6 +7,7 @@ namespace SpriteKind {
 sprites.onOverlap(SpriteKind.Enemy2, SpriteKind.公主, function (sprite, otherSprite) {
     info.changeLifeBy(-1)
     monster2.destroy()
+    scene.cameraShake(6, 500)
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     if (position != 90) {
@@ -18,6 +19,7 @@ sprites.onOverlap(SpriteKind.怪物攻擊, SpriteKind.Player, function (sprite, 
     info.changeLifeBy(-1)
     mySprite3.destroy()
     music.pewPew.play()
+    scene.cameraShake(6, 500)
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     mySprite2.destroy()
@@ -28,25 +30,26 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, oth
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.公主, function (sprite, otherSprite) {
     info.changeLifeBy(-1)
     mySprite2.destroy()
+    scene.cameraShake(6, 500)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.setImage(img`
-. . . . . . . f f . . . . . . . . . . . . . . . 
-. . . . f f f f 2 f f . . . . . . . . . . . . . 
-. . f f e e e e f 2 f f . . . . . . . . . . . . 
-. f f e e e e e f 2 2 f f . . . . . . . . . . . 
-. f e e e e f f e e e e f . . . . . . . . . . . 
-. f f f f f e e 2 2 2 2 e f . . . . . . . . . . 
-f f f e 2 2 2 f f f f e 2 f . . . . . . . . . . 
-f f f f f f f f e e e f f f . . . . . . . . . . 
-f e f e 4 4 e b f 4 4 e e f . . . . . . . . . . 
-. f e e 4 d 4 b f d d e f . . . . . . . . . . . 
-. . f e e e 4 d d d e e . c . . . . . . . . . . 
-. . . f 2 2 2 2 e e d d e c c c c c c c . . . . 
-. . . f 4 4 4 e 4 4 d d e c d d d d d . . . . . 
-. . . f f f f f e e e e . c c c c c . . . . . . 
-. . f f f f f f f f . . . c . . . . . . . . . . 
-. . f f f . . f f . . . . . . . . . . . . . . . 
+. f f f f f . f f . . . . . . . . . . . . . . . 
+f f d d d f f f e f f . . . . . . . . . . . . . 
+f e f f d d f e e e f f . . . . . . . . . . . . 
+f e e f f f e e e e e e f . . . . . . . . . . . 
+f f e e e e e e e e e e e f f . . . . . . . . . 
+. f e e e e e e e e e e e e f . . . . . . . . . 
+f f e e e e e f f f f e e f . . . . . . . . . . 
+f f f f f f f f e e e f f f . . . 2 . . . 2 2 . 
+f e f e 4 4 e b f 4 4 e e f . . . 2 2 . 2 5 2 . 
+. f e e 4 d 4 b f d d e f . f f f . 2 2 4 2 5 2 
+. . f e e e 4 d d d e e . 2 2 f f f f f f f 4 5 
+. . . f 8 5 8 8 e e d d e f f e e e e e e f 4 5 
+. . . f 8 8 8 e 4 4 d d e f e e e f f f f f 4 5 
+. . . f 8 8 8 8 e e e e f f e e e f . 2 4 4 5 5 
+. . f 8 8 f f 8 8 f . . f e e e f f 2 2 5 5 2 2 
+. . f f f . . f f . . . f f f f f 2 . . 2 2 . . 
 . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . 
@@ -77,22 +80,22 @@ f e f e 4 4 e b f 4 4 e e f . . . . . . . . . .
     music.pewPew.play()
     pause(1000)
     mySprite.setImage(img`
-. . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . f f f f . . . . . . . . . . . . . . . 
-. . . f f f 2 2 f f f . . . . . . . . . . . . . 
-. . f f f 2 2 2 2 f f f . . . . . . . . . . . . 
-. f f f e e e e e e f f f . . . . . . . . . . . 
-. f f e 2 2 2 2 2 2 e e f . . . . . . . . . . . 
-. f e 2 f f f f f f 2 e f . . . . . . . . . . . 
-. f f f f e e e e f f f f . . . . . . . . . . . 
+. . . . . . . . . . . f f f . . . . . . . . . . 
+. . . . . f f f f . f d d f . . . . . . . . . . 
+. . . f f e e e e f f d f . f f . . . . . . . . 
+. . f e e e e e e f d d f f e f . . . . . . . . 
+. f f f f f f f f f f f e e e f . . . . . . . . 
+. f e e e e e e e e e e e e f . . . . . . . . . 
+f e e e e e e e e e e e e f . . . . . . . . . . 
+. f f f f f f f f f f f f . . . . . . . . . . . 
 f f e f b f 4 4 f b f e f f . . . . . . . . . . 
 f e e 4 1 f d d f 1 4 e e f . . . . . . . . . . 
 . f f f f d d d d d e e f . . . . . . . . . . . 
-f d d d d f 4 4 4 e e f . . . . . . . . . . . . 
-f b b b b f 2 2 2 2 f 4 e . . . . . . . . . . . 
-f b b b b f 2 2 2 2 f d 4 . . . . . . . . . . . 
-. f c c f 4 5 5 4 4 f 4 4 . . . . . . . . . . . 
-. . f f f f f f f f . . . . . . . . . . . . . . 
+f d d d d f 8 8 8 8 e f . . . . . . . . . . . . 
+f b b b b f 8 8 5 8 f 4 e . . . . . . . . . . . 
+f b b b b f 8 8 8 8 f d 4 . . . . . . . . . . . 
+. f c c f 8 f f 8 8 f 4 4 . . . . . . . . . . . 
+. . f f f 8 8 8 8 8 . . . . . . . . . . . . . . 
 . . . . f f . . f f . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . 
@@ -234,22 +237,22 @@ _6.setPosition(5, 90)
 info.setLife(3)
 position = 0
 mySprite = sprites.create(img`
-. . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . f f f f . . . . . . . . . . . . . . . 
-. . . f f f 2 2 f f f . . . . . . . . . . . . . 
-. . f f f 2 2 2 2 f f f . . . . . . . . . . . . 
-. f f f e e e e e e f f f . . . . . . . . . . . 
-. f f e 2 2 2 2 2 2 e e f . . . . . . . . . . . 
-. f e 2 f f f f f f 2 e f . . . . . . . . . . . 
-. f f f f e e e e f f f f . . . . . . . . . . . 
+. . . . . . . . . . . f f f . . . . . . . . . . 
+. . . . . f f f f . f d d f . . . . . . . . . . 
+. . . f f e e e e f f d f . f f . . . . . . . . 
+. . f e e e e e e f d d f f e f . . . . . . . . 
+. f f f f f f f f f f f e e e f . . . . . . . . 
+. f e e e e e e e e e e e e f . . . . . . . . . 
+f e e e e e e e e e e e e f . . . . . . . . . . 
+. f f f f f f f f f f f f . . . . . . . . . . . 
 f f e f b f 4 4 f b f e f f . . . . . . . . . . 
 f e e 4 1 f d d f 1 4 e e f . . . . . . . . . . 
 . f f f f d d d d d e e f . . . . . . . . . . . 
-f d d d d f 4 4 4 e e f . . . . . . . . . . . . 
-f b b b b f 2 2 2 2 f 4 e . . . . . . . . . . . 
-f b b b b f 2 2 2 2 f d 4 . . . . . . . . . . . 
-. f c c f 4 5 5 4 4 f 4 4 . . . . . . . . . . . 
-. . f f f f f f f f . . . . . . . . . . . . . . 
+f d d d d f 8 8 8 8 e f . . . . . . . . . . . . 
+f b b b b f 8 8 5 8 f 4 e . . . . . . . . . . . 
+f b b b b f 8 8 8 8 f d 4 . . . . . . . . . . . 
+. f c c f 8 f f 8 8 f 4 4 . . . . . . . . . . . 
+. . f f f 8 8 8 8 8 . . . . . . . . . . . . . . 
 . . . . f f . . f f . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . 
